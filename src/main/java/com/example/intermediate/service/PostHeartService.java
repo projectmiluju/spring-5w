ac4a7758b73f946a433f4aad679feffcbfe74565
nohwooyeol <dnfdufdl@naver.com>
2022-08-05 17:16:10 +0900

    좋아요 서비스 게시글 존재여부, 회원여부 확인 추가

--- src/main/java/com/example/intermediate/service/PostHeartService.java
+++ src/main/java/com/example/intermediate/service/PostHeartService.java
@@ -1,9 +1,12 @@
 package com.example.intermediate.service;
 
 import com.example.intermediate.controller.request.PostHeartRequestDto;
+import com.example.intermediate.domain.Member;
 import com.example.intermediate.domain.Post;
 import com.example.intermediate.domain.PostHeart;
+import com.example.intermediate.repository.MemberRepository;
 import com.example.intermediate.repository.PostHeartRepository;
+import com.example.intermediate.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -16,9 +19,22 @@ public class PostHeartService {
 
     private final PostHeartRepository postHeartRepository;
 
+    private final PostRepository postRepository;
+
+    private final MemberRepository memberRepository;
+
     public PostHeart createPostHeart(PostHeartRequestDto requestDto, UserDetails userDetails) {
         // 게시글이 존재하는 지 여부
-
+         Optional<Post> postCheck = postRepository.findById(requestDto.getPostId());
+         if(postCheck.isEmpty()){
+             throw new IllegalArgumentException("게시글이 없습니다!");
+         }
+        
+         //회원인지 확인
+         Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
+         if (membercheck.isEmpty()){
+             throw new IllegalArgumentException("회원이 아닙니다");
+         }
 
 
         // 사용자가 같은 게시글에 중복으로 눌렀는지 검사
@@ -26,7 +42,7 @@ public class PostHeartService {
                 requestDto.getPostId(), requestDto.getMemberId());
 
         if (optionalPostHeart.isPresent()) {
-            return null;
+            throw new IllegalArgumentException("좋아요를 누르셨습니다!");
         }
 
         PostHeart postHeart = new PostHeart(requestDto);
