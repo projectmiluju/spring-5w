05f56e2370e070aab002634da60f3cc889425a3d
projectmiluju <dnjsdyd0712@naver.com>
2022-08-06 15:34:25 +0900

    게시글 좋아요 취소 예외 처리

--- src/main/java/com/example/intermediate/service/PostHeartService.java
+++ src/main/java/com/example/intermediate/service/PostHeartService.java
@@ -26,20 +26,20 @@ public class PostHeartService {
 
     public ResponseDto<?> createPostHeart(PostHeartRequestDto requestDto, UserDetails userDetails) {
         // 게시글이 존재하는 지 여부
-         Optional<Post> postCheck = postRepository.findById(requestDto.getPostId());
-         if(postCheck.isEmpty()){
-             throw new IllegalArgumentException("게시글이 없습니다!");
-         }
-        
-         //회원인지 확인
-         Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
-         if (membercheck.isEmpty()){
-             throw new IllegalArgumentException("회원이 아닙니다");
-         }
+        Optional<Post> postCheck = postRepository.findById(requestDto.getPostId());
+        if (postCheck.isEmpty()) {
+            throw new IllegalArgumentException("게시글이 없습니다!");
+        }
+
+        //회원인지 확인
+        Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
+        if (membercheck.isEmpty()) {
+            throw new IllegalArgumentException("회원이 아닙니다");
+        }
         //회원이 일치하는지 확인하기
-         if(!userDetails.getUsername().equals(membercheck.get().getNickname())){
-             throw new IllegalArgumentException("일치하는 회원이 아닙니다");
-         }
+        if (!userDetails.getUsername().equals(membercheck.get().getNickname())) {
+            throw new IllegalArgumentException("일치하는 회원이 아닙니다");
+        }
 
         // 사용자가 같은 게시글에 중복으로 눌렀는지 검사
         Optional<PostHeart> optionalPostHeart = postHeartRepository.findByPostIdAndMemberId(
@@ -58,19 +58,16 @@ public class PostHeartService {
 
         Optional<PostHeart> optionalPostHeart = postHeartRepository.findByPostIdAndMemberId(
                 requestDto.getPostId(), requestDto.getMemberId());
-        if (optionalPostHeart.isPresent()){
-
-            Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
-
-            if(!userDetails.getUsername().equals(membercheck.get().getNickname())){
-                throw new IllegalArgumentException("일치하는 회원이 아닙니다");
-            }
-
-            postHeartRepository.delete(optionalPostHeart.get());
-
-            return ResponseDto.success("좋아요를 지우셨습니다.");
+        //좋아요가 눌려있지 않다면
+        if (optionalPostHeart.isEmpty()) {
+            throw new IllegalArgumentException("좋아요를 누르지 않으셨습니다.");
         }
-
-        return ResponseDto.fail("BAD_REQUEST","좋아요를 누르지 않으셨습니다.");
+        Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
+        //누른게 로그인한 사람이 아니라면
+        if (!userDetails.getUsername().equals(membercheck.get().getNickname())) {
+            throw new IllegalArgumentException("일치하는 회원이 아닙니다");
+        }
+        postHeartRepository.delete(optionalPostHeart.get());
+        return ResponseDto.success("좋아요를 지우셨습니다.");
     }
 }
