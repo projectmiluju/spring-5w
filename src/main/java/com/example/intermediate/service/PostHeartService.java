8c16f107dc67d446e6235480c1fce0da0d9f9823
Kimilm <gnlfla7416@gmail.com>
2022-08-08 16:04:15 +0900

    수정 - 게시글 좋아요 삭제시 좋아요 누른 멤버 정보 연관관계를 통해 가져오도록 변경

--- src/main/java/com/example/intermediate/service/PostHeartService.java
+++ src/main/java/com/example/intermediate/service/PostHeartService.java
@@ -69,9 +69,9 @@ public class PostHeartService {
         if (optionalPostHeart.isEmpty()) {
             throw new IllegalArgumentException("좋아요를 누르지 않으셨습니다.");
         }
-        Optional<Member> membercheck = memberRepository.findById(requestDto.getMemberId());
+        Member membercheck = optionalPostHeart.get().getMember();
         //누른게 로그인한 사람이 아니라면
-        if (!userDetails.getUsername().equals(membercheck.get().getNickname())) {
+        if (!userDetails.getUsername().equals(membercheck.getNickname())) {
             throw new IllegalArgumentException("일치하는 회원이 아닙니다");
         }
         postHeartRepository.delete(optionalPostHeart.get());
