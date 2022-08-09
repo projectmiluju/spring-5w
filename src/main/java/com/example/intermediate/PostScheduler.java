af93bb1040aec614dbe3bf5ea88eba4fe537bcc8
Kimilm <gnlfla7416@gmail.com>
2022-08-09 11:57:36 +0900

    생성 - 댓글 없는 게시글 삭제 스케줄링

---
+++ src/main/java/com/example/intermediate/PostScheduler.java
@@ -0,0 +1,44 @@
+package com.example.intermediate;
+
+import com.example.intermediate.domain.Post;
+import com.example.intermediate.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PostScheduler {
+
+    private final PostRepository postRepository;
+
+    // 매일 새벽 1시
+    @Scheduled(cron = "0 0 1 * * *")
+    public void garbageCollect() {
+        List<Post> posts = postRepository.findNoComment();
+
+        for (Post post : posts) {
+            log.info("게시글 [id: " + post.getId() + ", title: " + post.getTitle() + "] 에 댓글이 없어 삭제되었습니다.");
+            postRepository.delete(post);
+        }
+    }
+
+//    // 매일 새벽 1시
+//    @Scheduled(cron = "0 0 1 * * *")
+//    @Transactional
+//    public void garbageCollectCheckComment() {
+//        List<Post> posts = postRepository.findAll();
+//
+//        for (Post post : posts) {
+//            if (post.getComments().isEmpty()) {
+//                log.info("게시글 [id: " + post.getId() + ", title: " + post.getTitle() + "] 에 댓글이 없어 삭제되었습니다.");
+//                postRepository.delete(post);
+//            }
+//        }
+//    }
+}
--- src/main/java/com/example/intermediate/repository/PostRepository.java
+++ src/main/java/com/example/intermediate/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.intermediate.repository;
 
 import com.example.intermediate.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,5 +11,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByMember_Id(Long MemberId);
 
-
+    @Query(value = "Select p from Post p Left Join Comment c On p.id = c.post.id where c.id is null")
+    List<Post> findNoComment();
 }
